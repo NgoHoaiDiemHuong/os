b5f1091724d34826dfefa3c034158dc3f26b15f9
NgoHoaiDiemHuong <ngohoaidiemhuong.1994@gmail.com>
2017-03-27 14:08:30 +0700

    install ovpn

---
+++ install_ulti.sh
@@ -0,0 +1,8 @@
+# install ovpn
+# http://askubuntu.com/questions/760664/ubuntu-16-04-openvpn
+sudo apt-get -y install network-manager-openvpn\
+                        network-manager-openvpn-gnome\
+                        network-manager-pptp\
+                        network-manager-vpnc
+
+
