be2a8508a08c2d6493924f6dc7f3202105e27ade
NgoHoaiDiemHuong <ngohoaidiemhuong.1994@gmail.com>
2017-03-27 14:32:03 +0700

    Update install_ulti.sh
    create key-gen

--- install_ulti.sh
+++ install_ulti.sh
@@ -5,4 +5,6 @@ sudo apt-get -y install network-manager-openvpn\
                         network-manager-pptp\
                         network-manager-vpnc
 
-
+# Generating Your SSH Public Key
+ssh-keygen -t rsa -C "ngohoaidiemhuong.1994@gmail.com"
+ssh-keygen -t rsa -C "huongnhd@evolableasia.vn"
