57d31b622efb910db1234c6cc99c7e5f1e3775ae
Ngo Hoai Diem Huong <huong.nhdh@gmail.com>
2019-02-28 21:58:58 +0700

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+: '
+@author: huongnhdh@gmail.com
+'
+set -euo pipefail
+
+for i in "$@"
+do
+case $i in
+    -e=*|--email=*)
+    EMAIL="${i#*=}"
+    ;;
+    -u=*|--user_name=*)
+    USER_NAME="${i#*=}"
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+echo USER_NAME = ${USER_NAME} || 'huongnhd'
+echo EMAIL = ${EMAIL} || 'huongnhdh@gmail.com'
+
+# generate key
+ssh-keygen -t rsa -C $USER_NAME
+
+# copy to clipboard
+xclip -sel c < .ssh/id_rsa.pub
+cat .ssh/id_rsa.pub | xclip -i 
+
+# git config user.name $USER_NAME
+git config --global user.name $USER_NAME
+git config --global user.email $EMAIL
+git config --global color.ui false
