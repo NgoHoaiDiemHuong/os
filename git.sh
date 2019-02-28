270b7cc7ee44cfb1dd8b216d2b4bd4919349c7a9
Ngo Hoai Diem Huong <huong.nhdh@gmail.com>
2019-02-28 23:10:17 +0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-: '
-@author: huongnhdh@gmail.com
-'
-set -euo pipefail
-IFS=$'\n\t'
+#!/bin/sh
+#: '
+# author: huongnhdh@gmail.com
+#'
+# set -xeuo pipefail
+# IFS=$'\n\t'
 
 for i in "$@"
 do
 case $i in
     -e=*|--email=*)
-    EMAIL="${i#*=}"
+    EMAIL=${i#*=}
     ;;
     -u=*|--user_name=*)
     USER_NAME="${i#*=}"
@@ -19,19 +19,28 @@ case $i in
     ;;
 esac
 done
+#if [ ! -n $USER_NAME ]
+#then 
+#  $USER_NAME="huongnhd" 
+#fi
+# set default value
 ${USER_NAME:='huongnhd'}
 ${EMAIL:='huongnhdh@gmail.com'}
-echo USER_NAME = ${USER_NAME}
-echo EMAIL = ${EMAIL}
+echo "USER_NAME = ${USER_NAME}"
+echo "EMAIL = ${EMAIL}"
 
 # generate key
 ssh-keygen -t rsa -C ${USER_NAME}
 
 # copy to clipboard
-xclip -sel c < .ssh/id_rsa.pub
-cat .ssh/id_rsa.pub | xclip -i 
+xclip -sel c < ${HOME}/.ssh/id_rsa.pub
+cat ${HOME}/.ssh/id_rsa.pub | xclip -i 
 
 # git config user.name ${USER_NAME}
 git config --global user.name ${USER_NAME}
 git config --global user.email ${EMAIL}
-git config --global color.ui false
+git config --global color.ui true
+
+# set up git alias
+curl -o ${HOME}/.gitalias.txt https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt
+git config --global include.path ${HOME}/.gitalias.txt
