b9782e62483eb62cf2845abcd980b7746ba7b96a
Ngo Hoai Diem Huong <huong.nhdh@gmail.com>
2019-05-11 15:33:20 +0700

    Create install_python3.sh

---
+++ install_python3.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+# author: huongnhd<huong.nhdh@gmail.com>
+# workon ubuntu
+# checklist
+# - python-dev-lib
+# - python3 pip
+# - virtualenv
+# - virtualenv wrapper
+# - jupiter note-book
+# - some lib commom
+set -e 
+
+# TODO://
